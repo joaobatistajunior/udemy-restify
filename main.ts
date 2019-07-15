3ecdaf6903e056d682ced5139f8dd3757b3d3a15
Junior <JoaoBatista.Junior@thomsonreuters.com>
2019-07-15 14:08:33 -0300

    First resource  - User

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
+import { usersRouter } from './users/users.router';
 import { Server } from "./server/server";
 
 const server = new Server();
 
-server.bootstrap().then(server => {
+server.bootstrap([usersRouter]).then(server => {
     console.log('Server is listening on:', server.application.address());
 }).catch(error => {
     console.log('Server failed to start');
