1c30c77e17bb5838cfb77805565dc55f37158383
Junior <JoaoBatista.Junior@thomsonreuters.com>
2019-07-15 12:09:37 -0300

    Exploring the request, response and next objects

--- main.ts
+++ main.ts
@@ -5,11 +5,33 @@ const server = restify.createServer({
     version: '1.0.0'
 });
 
-server.get('/hello', (req, resp, next)=>{
-    resp.json({message: 'hello'});
-    return next();
-});
+server.use(restify.plugins.queryParser());
 
-server.listen(3000, ()=>{
+server.get('/info', [(req, resp, next) => {
+    if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+        // resp.status(400);
+        // resp.json({ message: 'Please, update your browser' });
+        let error: any = new Error();
+        error.statusCode = 400;
+        error.message = 'Please, update your browser';
+        return next(error);
+    }
+    return next();
+}, (req, resp, next) => {
+    // resp.contentType = 'application/json';
+    // resp.status(400);
+    // resp.setHeader('Content-Type','application/json');
+    // resp.send({message: 'hello'});
+    resp.json({
+        browser: req.userAgent(),
+        method: req.method,
+        url: req.url,
+        path: req.path(),
+        query: req.query
+    });
+    return next();
+}]);
+
+server.listen(3000, () => {
     console.log('API is running on http://localhost:3000')
 });
