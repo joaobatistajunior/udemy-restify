9b3978d92f4b4d3a7821a5aaa32267460710ad18
Junior <JoaoBatista.Junior@thomsonreuters.com>
2019-07-29 09:16:15 -0300

    API version control - Restify

--- users/users.router.ts
+++ users/users.router.ts
@@ -1,5 +1,4 @@
 import * as restify from 'restify';
-import { NotFoundError } from 'restify-errors';
 import { ModelRouter } from '../common/model-router';
 import { User } from './users.model';
 
@@ -13,8 +12,27 @@ class UsersRouter extends ModelRouter<User> {
         });
     }
 
+    findByEmail = (req, resp, next) => {
+        if (req.query.email) {
+            User.find({ email: req.query.email })
+                .then(this.renderAll(resp, next))
+                .catch(next);
+        } else {
+            next();
+        }
+    }
+
     applyRoutes(application: restify.Server) {
-        application.get('/users', this.findAll);
+        application.get('/users', restify.plugins.conditionalHandler([
+            {
+                version: '1.0.0',
+                handler: this.findAll
+            },
+            {
+                version: '2.0.0',
+                handler: [this.findByEmail, this.findAll]
+            }
+        ]))
         application.get('/users/:id', [this.validateId, this.findById]);
         application.post('/users', this.save);
         application.put('/users/:id', [this.validateId, this.replace]);
