571cec5d5c3f8b4ed2d68f7703862e9aeebeaacc
Junior <JoaoBatista.Junior@thomsonreuters.com>
2019-08-30 18:43:38 -0300

    Pagination - Skip and Limit

--- common/model-router.ts
+++ common/model-router.ts
@@ -6,6 +6,8 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
 
     basePath: string;
 
+    pageSize: number = 2;
+
     constructor(protected model: mongoose.Model<D>) {
         super();
         this.basePath = `/${model.collection.name}`;
@@ -21,6 +23,26 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
         return resource;
     }
 
+    envelopeAll(documents: any[], options: any = {}): any {
+        const resource: any = {
+            _links: {
+                self: `${options.url}`,
+                totalElements: `${options.count}`
+            },
+            items: documents
+        };
+        if (options.page && options.count && options.pageSize) {
+            if (options.page > 1) {
+                resource._links.previous = `${this.basePath}?_page=${options.page - 1}`;
+            }
+            const remaining = options.count - (options.page * options.pageSize);
+            if (remaining > 0) {
+                resource._links.next = `${this.basePath}?_page=${options.page + 1}`;
+            }
+        }
+        return resource;
+    }
+
     validateId = (req, resp, next) => {
         if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
             next(new NotFoundError('Document not found'));
@@ -30,9 +52,20 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
     }
 
     findAll = (req, resp, next) => {
+        let page = parseInt(req.query._page || 1);
+        page = page > 0 ? page : 1;
+        const skip = (page - 1) * this.pageSize;
+
         this.model
-            .find()
-            .then(this.renderAll(resp, next))
+            .countDocuments({})
+            .exec()
+            .then(count =>
+                this.model
+                    .find()
+                    .skip(skip)
+                    .limit(this.pageSize)
+                    .then(this.renderAll(resp, next, { page, count, pageSize: this.pageSize, url: req.url }))
+            )
             .catch(next);
     };
 
